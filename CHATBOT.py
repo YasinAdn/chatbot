5d2cea6225a47d5e447785563899035b26adeb71
YasinAdn <72643868+YasinAdn@users.noreply.github.com>
2024-06-13 15:57:16 +0500

    Add files via upload

---
+++ CHATBOT.py
@@ -0,0 +1,19 @@
+#HELLO, WELCOME TO VELOCITY FAST FOOD
+
+menu = "1) PIZZA\n2) CHICKEN SANDWICH\n3) HOAGIE\n4) FALAFEL\n5) NACHOS\n6) POPCORN (SALTED, CARAMELIZED)\n7) ONION RINGS "
+
+name = input("Hello, how are you today.\n\nWhat is your name \n\n")
+
+print("\n Hello " + name + " welcome to Velocity Fast Foods, My name is ALYX, I will be taking your order ")
+
+print("\nHello " + name + ", Thank you for coming today \n\nWhat would you like from our menu\n") 
+
+print(menu) 
+
+order = input()
+
+print("\nSounds good " + name + ", We'll have the " + order + " ready for you :)")
+
+print("\nYour order of " + order + " will arrive in 10 minutes to your table") 
+
+print("\nWe thank you for your patronage :) ")
