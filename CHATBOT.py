0e2a6e5fae9e06819c0439b49f62379849e72b46
YasinAdn <72643868+YasinAdn@users.noreply.github.com>
2025-11-05 23:25:34 +0500

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -1,4 +1,4 @@
-#HELLO, WELCOME TO VELOCITY FAST FOOD
+#HELLO, WELCOME TO XYZ FOODs
 
 menu = "1) PIZZA\n2) CHICKEN SANDWICH\n3) HOAGIE\n4) FALAFEL\n5) NACHOS\n6) POPCORN (SALTED, CARAMELIZED)\n7) ONION RINGS "
 
@@ -17,4 +17,5 @@ print("\nSounds good " + name + ", We'll have the " + order + " ready for you :)
 print("\nYour order of " + order + " will arrive in 10 minutes to your table") 
 
 print("\nWe thank you for your patronage :) ")
+
 
